1888f75fde50a436516a21d0b7f81b674f2fad3a
fenixoleg7 <fenixoleg7@gmail.com>
2021-03-15 20:51:19 +0200

    feat: add solution for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let chars = str.split(''),
+        stack = [],
+        open = [],
+        close = [],
+        closeIndex,
+        openIndex;
+
+        bracketsConfig.forEach(function(item) {
+          open.push(item[0]);
+          close.push(item[1]);
+        });
+
+        if(chars.length%2 !== 0){
+          return false;
+        }
+           
+        for (var i = 0, len = chars.length; i < len; i++) {
+          openIndex = open.indexOf(chars[i]);
+          if (openIndex !== -1) {
+              stack.push(openIndex);
+          }
+
+          closeIndex = close.indexOf(chars[i]);
+
+          if (closeIndex !== -1) {
+              openIndex = stack.pop();
+              if (closeIndex !== openIndex) {
+                  return false;
+              }
+          }
+        }
+
+        if (stack.length > 0) {
+          return false;
+        }else{
+          return true;
+        }
 }
